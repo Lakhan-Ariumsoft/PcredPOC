d76fb0a3f998ac458b15868c21b6f5b78b1ddc6c
Lakhan-Ariumsoft <lakhan.shrivastav@ariumsoft.com>
2026-05-19 18:37:11 +0530

    Lakhan s | code for CMA false

--- app/main.py
+++ app/main.py
@@ -51,7 +51,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],       # e.g. ["https://yourapp.com"] in production
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
